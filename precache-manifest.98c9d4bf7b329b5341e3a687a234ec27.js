5800496f550436bb9e0e4ed16e3f2387e8fefaee
JAYKALIA007 <jaykalia047@gmail.com>
2020-07-22 01:56:56 +0530

    Updates

---
+++ precache-manifest.98c9d4bf7b329b5341e3a687a234ec27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b45cb3e37bd6032769b7768209d5523",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "72df6a2c6a1da681c9c5",
+    "url": "/movie-search-app/static/css/main.ef49d8da.chunk.css"
+  },
+  {
+    "revision": "3224c5d2dc734cbf9898",
+    "url": "/movie-search-app/static/js/2.c1453b77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.c1453b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72df6a2c6a1da681c9c5",
+    "url": "/movie-search-app/static/js/main.ee60a091.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
